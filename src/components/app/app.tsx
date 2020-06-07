8253a0bce64a47883dda4029aee515835df2063f
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-07 11:18:11 +0300

    add files.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
 
-function App() {
+const App: FC = () => {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
